542a308a0074529f6c2d6a464d78dbc78c877f8c
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-19 00:42:06 +0900

    chroma_execute関数の実装.cqt_censモードのみ.

--- DataProcessing.py
+++ DataProcessing.py
@@ -6,14 +6,23 @@ class DataProcessing():
     
     def __init__(self,file,mode):
         self.load_audio_data(file,mode)
+        self.hpss_execute()
+        self.chroma_execute('cqt_cens')
 
     def load_audio_data(self,file,mode):
         if mode=='a':
-            self.sr,self.loaded_data=wavfile.read(file)
+            sr,loaded_data=wavfile.read(file)
+            self.sr=sr
+            self.loaded_data=loaded_data.astype(np.float)
 
     def hpss_execute(self):
-        self.left_harmonics,self.left_percussive=librosa.effects.hpss(self.loaded_data[:,0])
-        self.right_harmonics,self.right_percussive=librosa.effects.hpss(self.loaded_data[:,1])
+        left_h,left_p=librosa.effects.hpss(self.loaded_data[:,0])
+        right_h,right_p=librosa.effects.hpss(self.loaded_data[:,1])
+
+        self.harmonics=np.stack([left_h,right_h],1)
+        self.percussive=np.stack([left_p,right_p],1)
+
+
 
     def chroma_execute(self,mode):
         N_BINS=48
@@ -21,9 +30,10 @@ class DataProcessing():
         FMIN=130.813
         WIN_LEN_SMOOTH=20
 
+        if mode=='cqt_cens':
+            C_left=librosa.cqt(self.harmonics[:,0],n_bins=N_BINS,hop_length=HOP_LENGTH,fmin=FMIN)
+            C_right=librosa.cqt(self.harmonics[:,1],n_bins=N_BINS,hop_length=HOP_LENGTH,fmin=FMIN)
+            left_chroma_cens=librosa.feature.chroma_cens(C=C_left,hop_length=HOP_LENGTH,fmin=FMIN,win_len_smooth=WIN_LEN_SMOOTH)
+            rihgt_chroma_cens=librosa.feature.chroma_cens(C=C_right,hop_length=HOP_LENGTH,fmin=FMIN,win_len_smooth=WIN_LEN_SMOOTH)
 
-        if mode=='cqt':
-            C_left=librosa.cqt(self.left_harmonics,n_bins=N_BINS,hop_length=HOP_LENGTH,fmin=FMIN)
-            C_right=librosa.cqt(self.right_harmonics,n_bins=N_BINS,hop_length=HOP_LENGTH,fmin=FMIN)
-            self.left_cens=librosa.feature.chroma_cens(C_left,hop_length=HOP_LENGTH,fmin=FMIN,win_len_smooth=WIN_LEN_SMOOTH)
-            self.rihgt_cens=librosa.feature.chroma_cens(C_right,hop_length=HOP_LENGTH,fmin=FMIN,win_len_smooth=WIN_LEN_SMOOTH)
+            self.chroma=np.stack([left_chroma_cens,rihgt_chroma_cens],0)
