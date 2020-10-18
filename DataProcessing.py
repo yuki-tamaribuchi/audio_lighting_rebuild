97f92bc3ea76ffb27e31776fdd304e8a53d45345
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-19 00:07:33 +0900

    chroma_execute関数の実装.cqtのみ

--- DataProcessing.py
+++ DataProcessing.py
@@ -14,3 +14,16 @@ class DataProcessing():
     def hpss_execute(self):
         self.left_harmonics,self.left_percussive=librosa.effects.hpss(self.loaded_data[:,0])
         self.right_harmonics,self.right_percussive=librosa.effects.hpss(self.loaded_data[:,1])
+
+    def chroma_execute(self,mode):
+        N_BINS=48
+        HOP_LENGTH=4096
+        FMIN=130.813
+        WIN_LEN_SMOOTH=20
+
+
+        if mode=='cqt':
+            C_left=librosa.cqt(self.left_harmonics,n_bins=N_BINS,hop_length=HOP_LENGTH,fmin=FMIN)
+            C_right=librosa.cqt(self.right_harmonics,n_bins=N_BINS,hop_length=HOP_LENGTH,fmin=FMIN)
+            self.left_cens=librosa.feature.chroma_cens(C_left,hop_length=HOP_LENGTH,fmin=FMIN,win_len_smooth=WIN_LEN_SMOOTH)
+            self.rihgt_cens=librosa.feature.chroma_cens(C_right,hop_length=HOP_LENGTH,fmin=FMIN,win_len_smooth=WIN_LEN_SMOOTH)
