2e2f95b53d332da67befdcdcb25e90884a57aa86
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-19 11:03:57 +0900

    テスト確認用変更

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 from DataProcessing import DataProcessing
+from Lighting import Lighting
 
 file = 'piano_only_bpm100_original.wav'
 mode = 'a'
 
+lt=Lighting('192.168.11.99')
 dp = DataProcessing(file,mode)
