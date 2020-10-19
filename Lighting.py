1afc898569ff46ae35a3e4904b6f15d7163bc80b
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-19 15:08:47 +0900

    for関連，cmd関連の修正と並列処理の追加

--- Lighting.py
+++ Lighting.py
@@ -1,5 +1,6 @@
 from phue import Bridge
 import time
+from multiprocessing import Process
 
 class Lighting():
     def __init__(self,ip_addr,audio_sec,color_data=None,brightness_data=None,left_lights=None,right_lights=None):
@@ -10,9 +11,9 @@ class Lighting():
         self.brightness_data=brightness_data
         self.left_lights=left_lights
         self.right_lights=right_lights
-
+        
         self.calc()
-
+        
 
     def calc(self):
         self.color_data_length=len(self.color_data[:,0])
@@ -22,14 +23,15 @@ class Lighting():
 
 
     def color(self):
-        for i in self.color_data_length:
+        for i in range(0,self.color_data_length):
             start=time.time()
+            
             cmd_left={
-                'xy':self.color_data[i,0],
+                'xy':(self.color_data[i,0,0],self.color_data[i,0,1]),
                 'transitiontime':0,
             }
             cmd_right={
-                'xy':self.color_data[i,1],
+                'xy':(self.color_data[i,1,0],self.color_data[i,1,1]),
                 'transitiontime':0,
             }
             self.b.set_light(self.left_lights,cmd_left)
@@ -38,16 +40,24 @@ class Lighting():
 
 
     def brightness(self):
-        for i in self.brightness_data_length:
+        for i in range(0,self.brightness_data_length):
             start=time.time()
             cmd_left={
-                'bri':self.brightness_data[i,0],
+                'bri':int(self.brightness_data[i,0]*255),
                 'transitiontime':0
             }
             cmd_right={
-                'bri':self.brightness_data[i,0],
+                'bri':int(self.brightness_data[i,0]*255),
                 'transitiontime':0
             }
             self.b.set_light(self.left_lights,cmd_left)
             self.b.set_light(self.right_lights,cmd_right)
             time.sleep(self.brightness_interval-(time.time()-start))
+
+    def execute(self):
+        processes=[
+            Process(target=self.color,),
+            Process(target=self.brightness,)
+        ]
+        for p in processes:
+            p.start()
