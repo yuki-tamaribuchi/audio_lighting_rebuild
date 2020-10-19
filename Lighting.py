f1040de13a823c0d9190446964bff34cd497d5d4
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2020-10-19 22:06:15 +0900

    color関数のfor文を変更

--- Lighting.py
+++ Lighting.py
@@ -20,28 +20,30 @@ class Lighting():
         self.brightness_data_length=len(self.brightness_data[:,0])
         self.color_interval=self.audio_sec/self.color_data_length
         self.brightness_interval=self.audio_sec/self.brightness_data_length
+        print(self.color_data.shape)
+        print(self.brightness_data.shape)
 
     def color(self):
 
         def left():
-            for i in range(0,self.color_data_length):
-                start=time.time()
 
-                cmd_left={
-                    'xy':(self.color_data[i,0,0],self.color_data[i,0,1]),
-                    'transitiontime':0,
+            for xy in self.color_data[:,0,:]:
+                start=time.time()
+                cmd={
+                    'xy':(xy[0],xy[1]),
+                    'transitiontime':0
                 }
-                self.b.set_light(self.left_lights,cmd_left)
+                self.b.set_light(self.left_lights,cmd)
                 time.sleep(self.color_interval-(time.time()-start))
 
         def right():
-            for i in range(0,self.color_data_length):
+            for xy in self.color_data[:,1,:]:
                 start=time.time()
-                cmd_right={
-                    'xy':(self.color_data[i,1,0],self.color_data[i,1,1]),
-                    'transitiontime':0,
+                cmd={
+                    'xy':(xy[0],xy[1]),
+                    'transitiontime':0
                 }
-                self.b.set_light(self.right_lights,cmd_right)
+                self.b.set_light(self.right_lights,cmd)
                 time.sleep(self.color_interval-(time.time()-start))
 
         processes=[
@@ -66,7 +68,7 @@ class Lighting():
                 time.sleep(self.brightness_interval-(time.time()-start))
 
         def right():
-            for i in range(0,self.brightness_interval):
+            for i in range(0,self.brightness_data_length):
                 start=time.time()
                 cmd_right={
                     'bri':int(self.brightness_data[i,0]*255),
