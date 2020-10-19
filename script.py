c16e49b20734ec5698e811cde24cd2d0226e9423
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-19 23:43:32 +0900

    ライト番号変更

--- script.py
+++ script.py
@@ -6,5 +6,5 @@ mode = 'a'
 
 
 dp = DataProcessing(file,mode)
-lt=Lighting('192.168.11.99',audio_sec=dp.audio_sec,color_data=dp.xy,brightness_data=dp.brightness,left_lights=3,right_lights=1)
+lt=Lighting('192.168.11.99',audio_sec=dp.audio_sec,color_data=dp.xy,brightness_data=dp.brightness,left_lights=3,right_lights=2)
 lt.execute()
