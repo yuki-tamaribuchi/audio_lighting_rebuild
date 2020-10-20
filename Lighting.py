9add49dd4807d5510ce3a2cc78fd46c66cff670d
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2020-10-20 20:05:05 +0900

    leftとrightの並列処理とそれぞれの関数内で並行処理をするよう変更

--- Lighting.py
+++ Lighting.py
@@ -22,6 +22,7 @@ class Lighting():
         self.color_interval=self.audio_sec/self.color_data_length
         self.brightness_interval=self.audio_sec/self.brightness_data_length
 
+    '''
     def color(self):
 
         def left():
@@ -90,5 +91,78 @@ class Lighting():
             Process(target=self.color,),
             Process(target=self.brightness,)
         ]
+        for p in processes:
+            p.start()
+    '''
+
+
+    def left(self):
+        def color():
+            for xy in self.color_data[:,0,:]:
+                start=time.time()
+                cmd={
+                    'xy':(xy[0],xy[1]),
+                    'transitiontime':0
+                }
+                Thread(target=self.b.set_light,args=(self.left_lights,cmd)).start()
+                end=time.time()
+                time.sleep(self.color_interval-(end-start))
+
+        def brightness():
+            for bri in self.brightness_data[:,0]:
+                start=time.time() 
+                cmd={
+                    'bri':int(bri*255),
+                    'transitiontime':0
+                }
+                Thread(target=self.b.set_light,args=(self.left_lights,cmd)).start()
+                end=time.time()
+                time.sleep(self.brightness_interval-(end-start))
+
+        threads=[
+            Thread(target=color),
+            Thread(target=brightness)
+        ]
+        
+        for t in threads:
+            t.start()
+
+    def right(self):
+        def color():
+            for xy in self.color_data[:,1,:]:
+                start=time.time()
+                cmd={
+                    'xy':(xy[0],xy[1]),
+                    'transitiontime':0
+                }
+                Thread(target=self.b.set_light,args=(self.right_lights,cmd)).start()
+                end=time.time()
+                time.sleep(self.color_interval-(end-start))
+
+        def brightness():
+            for bri in self.brightness_data[:,1]:
+                start=time.time()
+                cmd={
+                    'bri':int(bri*255),
+                    'transitiontime':0
+                }
+                Thread(target=self.b.set_light,args=(self.right_lights,cmd)).start()
+                end=time.time()
+                time.sleep(self.brightness_interval-(end-start))
+
+        threads=[
+            Thread(target=color),
+            Thread(target=brightness)
+        ]
+        
+        for t in threads:
+            t.start()
+
+    def execute(self):
+        processes=[
+            Process(target=self.left),
+            Process(target=self.right)
+        ]
+
         for p in processes:
             p.start()
