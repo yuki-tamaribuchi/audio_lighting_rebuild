0c5662389f8f4dc1459ff9a6deaa93ccd315b8c6
yuki-tamaribuchi <uk.tmbc2408@gmail.com>
2020-10-20 17:40:13 +0900

    テスト確認用変更

--- script.py
+++ script.py
@@ -9,4 +9,4 @@ mode = 'a'
 dp = DataProcessing(file,mode)
 lt=Lighting('192.168.11.99',audio_sec=dp.audio_sec,color_data=dp.xy,brightness_data=dp.brightness,left_lights=3,right_lights=1)
 player=Player(file,dp,lt)
-player.play()
+player.execute()
