1ab27705d38b609009db47285007382c3fc7557d
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2020-10-20 09:07:14 +0900

    play関数等の実装

--- Player.py
+++ Player.py
@@ -1,6 +1,15 @@
 import vlc
+import time
 
 class Player():
     
-    def __init__(self,audio_sec):
-        self.audio_sec=audio_sec
+    def __init__(self,file,data_instance,light_instance):
+        self.p=vlc.MediaPlayer()
+        self.p.set_mrl(file)
+        self.audio_sec=data_instance.audio_sec
+        self.light_instance=light_instance
+
+    def play(self):
+        self.light_instance.execute()
+        self.p.play()
+        time.sleep(self.audio_sec)
